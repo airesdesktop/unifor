3976e79470d874f18ce2564f33acab0a48a18813
airesdesktop <aires.dasilva@proton.me>
2026-09-06 19:17:59 -0300

    Created './algebra' and readme.md

---
+++ algebra/testes/operations.js
@@ -0,0 +1,17 @@
+    // imports
+import * as BasicOperations from "../classes/BasicOperations.js";
+const operations = new BasicOperations();
+
+    // valores para testes
+const matrizTeste = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+const vetorTeste = [1, 2, 3]
+
+    // operações
+console.log(operations.times(vetorTeste, matrizTeste))
+console.log(operations.dot(matrizTeste, matrizTeste))
+console.log(operations.sum(matrizTeste, matrizTeste))
+console.log(operations.transpose(matrizTeste))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# College assignments
+
+All assignments I made (or am making) for college, so ignore this <:0
